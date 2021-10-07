62acb07cd101cf29541a68ee591a36ad5cf5e9b2
Glitch (b-n-e) <none>
2021-10-07 09:29:07 +0000

    📺🎊 Checkpoint ./myApp.js:37009276/400

--- myApp.js
+++ myApp.js
@@ -2,9 +2,7 @@ var express = require("express");
 var app = express();
 
 console.log("Hello World");
-const aph=__dirname+"/views/index.html"
-const apu=__dirname+"/public"
-app.get("/", (req, res) => res.sendFile(aph));
-app.use("/public",express.static(apu))
+app.get("/", (req, res) => res.sendFile(__dirname+"/views/index.html"));
+app.use("/public",express.static(__dirname+"/public"))
 
 module.exports = app;
