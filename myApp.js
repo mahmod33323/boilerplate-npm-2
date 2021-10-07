5de41aacc64b1a2f4b8a1d766fbafc63e8c145e6
Glitch (b-n-e) <none>
2021-10-07 09:40:17 +0000

    🎹🎷 Checkpoint ./myApp.js:37009276/913

--- myApp.js
+++ myApp.js
@@ -4,5 +4,6 @@ var app = express();
 console.log("Hello World");
 app.get("/", (req, res) => res.sendFile(__dirname+"/views/index.html"));
 app.use("/public",express.static(__dirname+"/public"))
+app.get("/json",(req,res)=>res.json({"message":"Hello json"}))
 
 module.exports = app;
