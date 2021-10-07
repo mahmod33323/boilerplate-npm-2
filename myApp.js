29f9e643cb2540b8633f6982e4dd1ba78965d13c
Glitch (b-n-e) <none>
2021-10-07 10:02:23 +0000

    🍝🚴 Checkpoint ./myApp.js:37009276/1194

--- myApp.js
+++ myApp.js
@@ -4,6 +4,9 @@ var app = express();
 console.log("Hello World");
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/json", (req, res) => res.json({ message: "Hello json" }));
-console.log(process.env);
+app.get("/json", (req, res) => {
+  process.env.MESSAGE_STYLE == "uppercase"
+    ? res.json({ message: "HELLO JSON" })
+    : res.json({ message: "Hello json" });
+});
 module.exports = app;
