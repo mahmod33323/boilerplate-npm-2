72b76569574bdc1738f9e55bb947a98c28ff98b0
Glitch (b-n-e) <none>
2021-10-09 11:44:53 +0000

    🍭🌋 Checkpoint ./myApp.js:37009276/4547

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
@@ -11,11 +13,6 @@ console.log("Hello World");
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 app.use("/public", express.static(__dirname + "/public"));
-app.use("/", (req, res, n) => {
-  bodyParser.urlencoded({ extended: false });
-  console.log("bp");
-  n();
-});
 
 app.get("/json", (req, res) => {
   process.env.MESSAGE_STYLE == "uppercase"
@@ -41,4 +38,5 @@ app.get("/name", (req, res) => {
   const { first, last } = req.query;
   res.json({ name: first.concat(` ${last}`) });
 });
+
 module.exports = app;
